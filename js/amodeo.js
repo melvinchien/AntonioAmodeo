eb00bb14720a1150733250c7c3652f4c67433e18
suuoh <hello@melvin.io>
2014-03-24 22:16:51 -0400

    Basic template

--- js/amodeo.js
+++ js/amodeo.js
@@ -3,3 +3,14 @@
  * Copyright © 2014 Antonio Amodeo
  * Created by Melvin Chien melvin.io
  */
+
+ $(document).foundation({
+    orbit: {
+        animation: 'fade',
+        timer_speed: 5000,
+        resume_on_mouseout: true,
+        navigation_arrows: false,
+        slide_number: false,
+        bullets: false
+    }
+ });
