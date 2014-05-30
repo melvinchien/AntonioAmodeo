3d16b894281490c85255a7d56d07416dbbfb6492
suuoh <hello@melvin.io>
2014-05-29 23:37:25 -0700

    Add magnific popup

--- js/amodeo.js
+++ js/amodeo.js
@@ -37,6 +37,16 @@
         });
     });
 
+    $(".popup-youtube, .popup-vimeo").magnificPopup({
+        disableOn: 700,
+        type: "iframe",
+        mainClass: "mfp-fade",
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
+
     onResize();
     $(window).resize(onResize);
 });
